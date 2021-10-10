f675ca660270408f96b40102b7cb61247b057063
devkuros <agrahkun@gmail.com>
2021-10-10 21:08:57 +0700

    update menu

--- resources/views/components/admins/top-bar.blade.php
+++ resources/views/components/admins/top-bar.blade.php
@@ -86,9 +86,11 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                     </a>
                     <ul class="collapse submenu list-unstyled" id="suratmasuk" data-parent="#topAccordion">
-                        <li class="{{ set_active('category.index') }}">
-                            <a href="{{route('category.index')}}"> Kategori Surat </a>
-                        </li>
+                        @can('assign category')
+                            <li class="{{ set_active('category.index') }}">
+                                <a href="{{route('category.index')}}"> Kategori Surat </a>
+                            </li>
+                        @endcan
                         <li class="{{ set_active('masuks.index') }}">
                             <a href="{{route('masuks.index')}}"> Surat Masuk </a>
                         </li>
@@ -98,7 +100,7 @@
                     </ul>
                 </li>
 
-                <li class="menu single-menu {{ set_active('fakultas.index') }}{{ set_active('industri.index') }}">
+                <li class="menu single-menu {{ set_active('industri.keluar') }}{{ set_active('fakultas.keluar') }}{{ set_active('fakultas.index') }}{{ set_active('industri.index') }}">
                     <a href="#" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle autodroprown">
                         <div class="">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-folder"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
@@ -141,20 +143,27 @@
                 @endcan
 
                 @hasrole('super admin')
-                <li class="menu single-menu">
+                <li class="menu single-menu {{ set_active('admin.jabatan') }}{{ set_active('admin.pejabat') }}">
                     <a href="#" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle autodroprown">
                         <div class="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>                            <span>Admin Menu</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
+                            <span>Admin Menu</span>
                         </div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                     </a>
                     <ul class="collapse submenu list-unstyled" id="adminmeny" data-parent="#topAccordion">
-                        <li class="{{ set_active('admin.jabatan') }}">
-                            <a href="{{route('admin.jabatan')}}"> Jabatan </a>
+                        <li class="sub-sub-submenu-list {{ set_active('admin.jabatan') }}{{ set_active('admin.pejabat') }}">
+                            <a href="#starter-kit" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> Pejabat <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg> </a>
+                            <ul class="collapse list-unstyled sub-submenu eq-animated eq-fadeInUp" id="starter-kit" data-parent="#more">
+                                <li class="{{ set_active('admin.jabatan') }}">
+                                    <a href="{{route('admin.jabatan')}}"> Jabatan </a>
+                                </li>
+                                <li class="{{ set_active('admin.pejabat') }}">
+                                    <a href="{{route('admin.pejabat')}}"> Pejabat </a>
+                                </li>
+                            </ul>
                         </li>
-                        <li class="{{ set_active('admin.pejabat') }}">
-                            <a href="{{route('admin.pejabat')}}"> Pejabat </a>
-                        </li>
+
                     </ul>
                 </li>
                 @endhasrole
